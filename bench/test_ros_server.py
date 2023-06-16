a71b299a14a25296d6183d154dd7acbd53b5514f
Eric Chen <kych@berkeley.edu>
2023-06-16 17:06:24 +0000

    save instructions for running demo

--- bench/test_ros_server.py
+++ bench/test_ros_server.py
@@ -4,10 +4,10 @@ from time import sleep
 def send_request(
     api_op, 
     ros_op,
-    topic = "/chatter",
+    topic = "/topic",
     type = "std_msgs/msg/String", 
-    uri = "http://localhost:3000/add"
-
+    ip = "localhost", 
+    port = "3000"
 ):
     sleep(2)
     ros_topic = {
@@ -17,12 +17,30 @@ def send_request(
         "topic_name": topic,
         "topic_type": type,
     }
+    uri = f"http://{ip}:{port}/add"
     # Create a new resource
     response = requests.post(uri, json = ros_topic)
     print(response)
+ 
 
-send_request("add", "sub", topic = "/chatter2")
-send_request("add", "pub", topic = "/chatter2")  
-send_request("add", "sub")
+print("request Machine 1 (Azure) to publish chatter topic")
+send_request("add", "sub", ip = "172.190.80.56")
+
+print("locally publish chatter topic")
 send_request("add", "pub")
-send_request("del", "sub")
+
+for i in range(10):
+    sleep(1)
+    print(".")
+
+print("remove the topic of Machine 1's published topic")
+send_request("del", "sub", ip="172.190.80.56")
+print("have machine 2 (Another AWs) to publish the topic")
+send_request("add", "sub", ip="54.67.119.252")
+print("locally publish chatter topic")
+send_request("add", "pub")
+# migration!
+# print("remove the topic of Machine 1's published topic")
+# send_request("del", "sub", ip="172.190.80.56")
+# print("have machine 2 (Another AWs) to publish the topic")
+# send_request("add", "sub", ip="54.67.119.252")
