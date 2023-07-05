f518243003961e0f2d78701fafa866a7c01e2539
Eric Chen <kych@berkeley.edu>
2023-07-05 20:18:57 +0000

    sgc launchable

--- sgc_launch/setup.py
+++ sgc_launch/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'sgc_node = sgc_launch.sgc_node:main'
+            'sgc_router = sgc_launch.sgc_node:main'
         ],
     },
 )
--- sgc_launch/sgc_launch/sgc_node.py
+++ sgc_launch/sgc_launch/sgc_node.py
@@ -1,6 +1,15 @@
-def main():
-    print('Hi from sgc_launch.')
 
+
+import subprocess, os
+
+def build_sgc():
+    print("building FogROS sgc... It takes longer time for first time")
+    current_env = os.environ.copy()
+    current_env["PATH"] = f"/usr/sbin:/sbin:{current_env['PATH']}"
+    subprocess.Popen("cargo run --manifest-path ./src/fogros2-sgc-digial-double/Cargo.toml router", env=current_env,  shell=True)
+
+def main():
+    build_sgc()
 
 if __name__ == '__main__':
     main()
