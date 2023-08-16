064a6955b0d0d8f3d242cd10ac5d7142b90c05b5
Eric Chen <kych@berkeley.edu>
2023-08-16 03:05:09 +0000

    seems to work

--- sgc_launch/sgc_launch/sgc_policy_scheduler.py
+++ sgc_launch/sgc_launch/sgc_policy_scheduler.py
@@ -21,9 +21,11 @@ class SGC_Policy_Scheduler(rclpy.node.Node):
         super().__init__('sgc_policy_scheduler')
         self.logger = self.get_logger()
 
-        self.load_config_file()
+        self.machine_profile_dict = dict()
+        self.assignment_dict = dict()
 
-        self.machine_dict = dict()
+        self._load_config_file()
+        self._load_initial_state_assignment()
 
         # subscribe to the profile topic from other machines (if any)
         self.status_topic = self.create_subscription(
@@ -35,7 +37,17 @@ class SGC_Policy_Scheduler(rclpy.node.Node):
         # assignment service client to sgc_node
         self.assignment_service_client = self.create_client(SgcAssignment, 'sgc_assignment_service')
 
-    def load_config_file(self):
+        self.max_latency_bound = float(self.config["time_bound"]["max_latency"]) if "max_latency" in self.config["time_bound"] else -1
+        self.mean_latency_bound = float(self.config["time_bound"]["mean_latency"]) if "mean_latency" in self.config["time_bound"] else -1
+        self.median_latency_bound = float(self.config["time_bound"]["median_latency"]) if "median_latency" in self.config["time_bound"] else -1
+        self.min_latency_bound = float(self.config["time_bound"]["min_latency"]) if "min_latency" in self.config["time_bound"] else -1
+        self.std_latency_bound = float(self.config["time_bound"]["std_latency"]) if "std_latency" in self.config["time_bound"] else -1
+        # self.mean_latency_bound = float(self.config["time_bound"]["mean_latency"]) if self.config["time_bound"]["mean_latency"] else -1
+        # self.median_latency_bound = float(self.config["time_bound"]["median_latency"]) if self.config["time_bound"]["median_latency"] else -1
+        # self.min_latency_bound = float(self.config["time_bound"]["min_latency"]) if self.config["time_bound"]["min_latency"] else -1
+        # self.std_latency_bound = float(self.config["time_bound"]["std_latency"]) if self.config["time_bound"]["std_latency"] else -1
+
+    def _load_config_file(self):
         self.declare_parameter("config_path", "")
         self.config_path = self.get_parameter("config_path").value
 
@@ -45,7 +57,7 @@ class SGC_Policy_Scheduler(rclpy.node.Node):
         current_env = os.environ.copy()
         current_env["PATH"] = f"/usr/sbin:/sbin:{current_env['PATH']}"
         ws_path = current_env["COLCON_PREFIX_PATH"]
-        config_path = f"{ws_path}/sgc_launch/share/sgc_launch/configs" if not config_path else config_path
+        config_path = f"{ws_path}/sgc_launch/share/sgc_launch/configs" if not self.config_path else self.config_path
         config_yaml_file_path = f"{config_path}/{self.config_file_name}"
         with open(config_yaml_file_path, "r") as f:
             self.config = yaml.safe_load(f)
@@ -53,105 +65,106 @@ class SGC_Policy_Scheduler(rclpy.node.Node):
 
     def profile_topic_callback(self, profile_update):
         self.logger.info(f"received profile update from {profile_update}")
-        if profile_update.identity.data == self.identity:
-            # same update from its own publisher, we are only interested in other machine's
-            # updates 
-            return 
-        self.machine_dict[profile_update.identity.data] = profile_update
-        if profile_update.latency and not (((self.latency_df['timestamp'] == self.current_timestamp) & (self.latency_df['machine'] == profile_update.identity.data)).any()):
-            self.latency_df = pd.concat(
-                    [self.latency_df, pd.DataFrame([
-                        {
-                        "timestamp": self.current_timestamp,
-                        "latency": profile_update.latency,
-                        "machine": profile_update.identity.data,
-                        }
-                    ])], ignore_index=True
-                )
+        self.machine_profile_dict[profile_update.identity.data] = profile_update
 
+        # we assume the robot runs the scheduler for now
+        if profile_update.identity.data == self._get_robot_machine_name_from_state_assignment():
+            self._check_latency_bound(profile_update)
             
     # check if the latency is within the bound using heuristics
-    def _check_latency_bound(self):
-        
-        # get the latency from the dataframe for the last 15 seconds
-        # replace 0 with NaN so that it doesn't affect the average
-        # https://stackoverflow.com/questions/35608893/calculate-minimums-in-pandas-without-zero-values
-        self.latency_df_of_current_timestamps = self.latency_df[self.latency_df['timestamp'] > self.current_timestamp - 15].replace(0, np.NaN)
-        self.logger.info(f"latency at the past 15 second {self.latency_df_of_current_timestamps.to_string()}")
+    def _check_latency_bound(self, profile):
+        # max 
+        if self.max_latency_bound != -1 and profile.max_latency > self.max_latency_bound:
+            self.logger.info(f"max latency {profile.max_latency} is larger than the bound {self.max_latency_bound}")
+            self._switch_to_machine(self._get_machine_with_better_compute())
+        # mean
+        if self.mean_latency_bound != -1 and profile.mean_latency > self.mean_latency_bound:
+            self.logger.info(f"mean latency {profile.mean_latency} is larger than the bound {self.mean_latency_bound}")
+            self._switch_to_machine(self._get_machine_with_better_compute())
+        # median
+        if self.median_latency_bound != -1 and profile.median_latency > self.median_latency_bound:
+            self.logger.info(f"median latency {profile.median_latency} is larger than the bound {self.median_latency_bound}")
+            self._switch_to_machine(self._get_machine_with_better_compute())
+        # min
+        if self.min_latency_bound != -1 and profile.min_latency < self.min_latency_bound:
+            self.logger.info(f"min latency {profile.min_latency} is smaller than the bound {self.min_latency_bound}")
+            # TODO: here need to switch to a worse compute machine
+            self._switch_to_machine(self._get_machine_with_better_compute())
+        # std
+        if self.std_latency_bound != -1 and profile.std_latency > self.std_latency_bound:
+            self.logger.info(f"std latency {profile.std_latency} is larger than the bound {self.std_latency_bound}")
+            self._switch_to_machine(self._get_machine_with_better_compute())
 
-        # # compute latency is the smallest nonzero latency of the df
-        # compute_latency = self.latency_df_of_current_timestamps[self.latency_df_of_current_timestamps['latency'] > 0]['latency'].min()
-        self.logger.info(f"{self.latency_df_of_current_timestamps.groupby('machine')['latency'].mean()}")
-
-        # compute latency is the average latency of the df of the column that has smallest nonzero average latency
-        compute_latency = self.latency_df_of_current_timestamps.groupby('machine')['latency'].mean().min()
-        self.logger.info(f"compute latency {compute_latency}")
-
-        # end to end latency is the largest latency of the df
-        # end_to_end_latency = self.latency_df_of_current_timestamps['latency'].max()
-        # compute latency is the average latency of the df of the column that has largest average latency
-        end_to_end_latency = self.latency_df_of_current_timestamps.groupby('machine')['latency'].mean().max()
-        self.logger.info(f"end to end latency {end_to_end_latency}")
-
-        network_latency =  end_to_end_latency - compute_latency
-        self.logger.info(f"network latency {network_latency}")
-
-        need_better_compute = compute_latency > self.compute_latency_bound
-        need_better_network = network_latency > self.network_latency_bound
-        self.logger.info(f"need better compute {need_better_compute}, need better network {need_better_network}")
-        return need_better_compute, need_better_network
-    
     def _get_machine_with_better_compute(self):
         # get the best machine based on current spec collected 
         # param = optimize_compute vs optimize 
         # if machine has gpu 
         # else cpu_core >= cpu_core:
         # else cpu_frequency 
-        # for machine in self.machine_dict:
-        #     if self.machine_dict[machine].has_gpu and not self.has_gpu:
+        # for machine in self.machine_profile_dict:
+        #     if self.machine_profile_dict[machine].has_gpu and not self.has_gpu:
         #         return machine
-        #     if self.machine_dict[machine].num_cpu_core >= self.profile.num_cpu_core:
+        #     if self.machine_profile_dict[machine].num_cpu_core >= self.profile.num_cpu_core:
         #         return machine
-        #     if self.machine_dict[machine].cpu_frequency >= self.profile.cpu_frequency:
+        #     if self.machine_profile_dict[machine].cpu_frequency >= self.profile.cpu_frequency:
         #         return machine
         # return "machine_cloud" #TODO: currently it's hardcoded placeholder
-        self.logger.error("not implemented yet; need to switch to a compute")
-        pass
-    
+        # current_machine = self._get_service_machine_name_from_state_assignment()
+        standby_machines = self._get_standby_machine_list_from_state_assignment()
+        if len(standby_machines) == 1:
+            self.logger.info(f"switch to {standby_machines[0]} because it is the only standby machine")
+            return standby_machines[0]
+        for machine in standby_machines:
+            # this is a new machine that has not been profiled yet
+            if self.machine_profile_dict[machine].latency == 0:
+                self.logger.info(f"switch to {machine} because it has not been profiled yet")
+                return machine
     def _get_machine_with_best_network(self):
         # use ping to get the latency
         latency_dict = dict()
-        for machine in self.machine_dict:
-            network_info = ping_host(self.machine_dict[machine].ip_addr.data)
+        for machine in self.machine_profile_dict:
+            network_info = ping_host(self.machine_profile_dict[machine].ip_addr.data)
             self.logger.info(f"latency to {machine} is {network_info}")
             latency_dict[machine] = network_info["avg_latency"]
             # self.logger.info(f"latency to {machine} is {latency_dict[machine]}")
         return min(latency_dict, key=latency_dict.get)
 
-    def _switch_to_machine(self, machine):
-        if machine == self.identity:
-            # no need to switch
-            return
+    def _switch_to_machine(self, machine_new):
         
         request = SgcAssignment.Request()
-        request.machine.data = "machine_local"
-        request.state.data = "standby" # TODO: currently it's hardcoded
+        request.machine.data = self._get_service_machine_name_from_state_assignment()
+        request.state.data = "standby" 
         _ = self.assignment_service_client.call_async(request)
-
-
         
-        request.machine.data = "machine_cloud"
-        request.state.data = "service" # TODO: currently it's hardcoded
+        request.machine.data = machine_new
+        request.state.data = "service" 
         _ = self.assignment_service_client.call_async(request)
 
-    def get_initial_state_assignment(self):
+    def _load_initial_state_assignment(self):
         for identity_name in self.config["assignment"]:
             state_name = self.config["assignment"][identity_name]
-            if state_name not in self.state_dict:
-                self.logger.warn(f"State {state_name} not defined. Not added!")
-                continue 
             self.assignment_dict[identity_name] = state_name
+        self.logger.info(f"initial assignment is loaded {self.assignment_dict}")
         return self.assignment_dict
+
+    def _get_robot_machine_name_from_state_assignment(self):
+        for identity_name in self.assignment_dict:
+            if self.assignment_dict[identity_name] == "robot":
+                return identity_name
+        return None
+    
+    def _get_service_machine_name_from_state_assignment(self):
+        for identity_name in self.assignment_dict:
+            if self.assignment_dict[identity_name] == "service":
+                return identity_name
+        return None
+    
+    def _get_standby_machine_list_from_state_assignment(self):
+        standby_machine_list = []
+        for identity_name in self.assignment_dict:
+            if self.assignment_dict[identity_name] == "standby":
+                standby_machine_list.append(identity_name)
+        return standby_machine_list
     
 def main():
     rclpy.init()
